4e824be0cbebf66dbc453e585da1e832aaa1e16f
marxolly <mark@solly.com.au>
2022-07-12 11:26:40 +1000

    Update Config.php

--- app/core/Config.php
+++ app/core/Config.php
@@ -78,6 +78,7 @@ class Config{
      * @param mixed  $value
      */
     public static function setJsConfig($key, $value){
+        echo $key; return;
         self::_set($key, $value, self::$prefix['js']);
     }
 
