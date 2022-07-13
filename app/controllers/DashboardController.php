8cc0ee95e75fd4a75fe305ce779378843bdad0b9
marxolly <mark@solly.com.au>
2022-07-14 09:58:04 +1000

    home page

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -15,42 +15,18 @@ class DashboardController extends Controller
      */
     public function index()
     {
+        //die('index controller');
         $orders = array();
-        $production_orders = array();
-        $deliveries = array();
-        $pickups = array();
-        $backorders = array();
         $client_id = 0;
         $clients = array();
-        $user_role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
-        if($user_role == "admin" ||  $user_role == "warehouse")
-        {
-            $orders = $this->order->getCurrentOrders();
-            $backorders = $this->order->getCurrentBackorderOrders();
-            $deliveries = $this->delivery->getOpenDeliveryCount();
-            $pickups = $this->pickup->getOpenPickupCount();
-        }
-        elseif($user_role == 'client')
-        {
-            $client_id = $this->user->getUserClientId( Session::getUserId() );
-        }
-        //elseif( $user_role == "production admin" )
-        elseif( Session::isProductionUser() )
-        {
-            $production_orders = $this->order->getCurrentProductionOrders();
-        }
+        $user_role = (Session::isAdminUser())? 'admin' : Session::getUserRole();;
         Config::setJsConfig('curPage', "dashboard");
-        Config::set('curPage', "dashboard");
         $this->view->renderWithLayouts(Config::get('VIEWS_PATH') . "layout/dashboard/", Config::get('VIEWS_PATH') . 'dashboard/index.php',[
             'pht'                   =>  ": Home Page",
             'client_id'             =>  $client_id,
             'orders'                =>  $orders,
             'clients'               =>  $clients,
-            'user_role'             =>  $user_role,
-            'backorders'            =>  $backorders,
-            'production_orders'     =>  $production_orders,
-            'deliveries'            =>  $deliveries,
-            'pickups'               =>  $pickups
+            'user_role'             =>  $user_role
         ]);
     }
 
