3a6d7a4a72c9cf77fd86cd8e9d240ce856798471
marxolly <mark@solly.com.au>
2022-07-14 11:31:11 +1000

    Update index.php

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -21,10 +21,15 @@ $c = 1;
             <input type="hidden" id="admin_from_value" value="<?php echo strtotime('last friday', strtotime('-3 months'));?>" />
             <input type="hidden" id="admin_to_value" value="<?php echo strtotime('last friday', strtotime('tomorrow'));?>" />
             <div class="row">
-                <div class="row">
+                <div class="col-12">
                     <h1>Header 1</h1>
                 </div>
-
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <h2>Header 2</h2>
+                </div>
+            </div>
                 <h2>Header 2</h2>
                 <h3>Header 3</h3>
                 <h4>Header 4</h4>
