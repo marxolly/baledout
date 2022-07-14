1260fe1151c0a7958b4a7f43345c3cce7591a0f1
marxolly <mark@solly.com.au>
2022-07-14 16:21:14 +1000

    Update controllerIndex.php

--- app/views/index/controllerIndex.php
+++ app/views/index/controllerIndex.php
@@ -23,7 +23,7 @@ $resource = strtolower(str_replace(" ", "", $page_name));
                 $action = Utility::toCamelCase($SectionName);
                 //echo "<p>Checking Action: $action</p>";
                 if(Permission::check($role, $resource, $action, [], false)):?>
-                    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-3">
                         <div class="card">
                             <div class="card-header">
                                 <h4><?php echo $SectionName;?></h4>
