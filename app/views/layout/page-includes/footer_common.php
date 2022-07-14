48fb03a39a11fbcfa04c39d7563e51f67d8c6d5d
marxolly <mark@solly.com.au>
2022-07-14 14:49:50 +1000

    Update footer_common.php

--- app/views/layout/page-includes/footer_common.php
+++ app/views/layout/page-includes/footer_common.php
@@ -1,6 +1,6 @@
 <?php
-$date = (date('Y') == 2020)? date('Y') : "2020 - ".date('Y');
+$date = (date('Y') == 2022)? date('Y') : "2022 - ".date('Y');
 ?>
 <footer class="panel-footer clearfix" id="the_footer">
-    Copyright &copy; <?php echo $date;?>  FSG (Filmshot Graphics Pty Ltd)
+    Copyright &copy; <?php echo $date;?>  Baledout Pty Ltd
 </footer>
