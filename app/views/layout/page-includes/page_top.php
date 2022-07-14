3aa2451f453490bd9a3e6b358ced4fa79dd3c34d
marxolly <mark@solly.com.au>
2022-07-14 11:00:46 +1000

    Update page_top.php

--- app/views/layout/page-includes/page_top.php
+++ app/views/layout/page-includes/page_top.php
@@ -1,6 +1,11 @@
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header"><?php echo $page_title;?></h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
+<?php
+
+?>
+<?php include(Config::get('VIEWS_PATH')."layout/page-includes/breadcrumb.php");?>
+<?php if(isset($page_title)):?>
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 class="page-header" id="page_header"><?php echo $page_title;?></h2>
+        </div>
+    </div>
+<?php endif;?>
