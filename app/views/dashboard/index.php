499f08fc20ad6cf428be6dd780817ce8c792446b
marxolly <mark@solly.com.au>
2022-07-14 14:58:55 +1000

    Update index.php

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -13,7 +13,7 @@ $c = 1;
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Baledout Web Portal</h1>
+                <h2 class="page-header">Typography</h2>
             </div>
         </div>
         <?php //echo $user_role; die();?>
