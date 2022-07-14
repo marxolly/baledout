7e6594f52bd7f7861b13f982fd1ae29167ad1ef1
marxolly <mark@solly.com.au>
2022-07-14 16:18:35 +1000

    Update controllerIndex.php

--- app/views/index/controllerIndex.php
+++ app/views/index/controllerIndex.php
@@ -16,21 +16,24 @@ $resource = strtolower(str_replace(" ", "", $page_name));
 <div id="page-wrapper">
     <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
     <div id="page_container" class="container-xxl"> 
-        <div class="card-deck indexpagedeck">
+        <div class="row">
             <?php foreach($sections as $section_name => $details):
                 if(!isset($details['display']) || !$details['display']) continue;
                 $SectionName = ucwords(str_replace("-", " ", $section_name));
                 $action = Utility::toCamelCase($SectionName);
                 //echo "<p>Checking Action: $action</p>";
                 if(Permission::check($role, $resource, $action, [], false)):?>
-                    <div class="card indexpagecard">
-                        <div class="card-header">
-                            <h4><?php echo $SectionName;?></h4>
-                        </div>
-                        <div class="card-body text-center">
-                        	<a class="btn btn-lg btn-outline-fsg controller-index-link" href="/<?php echo $con_index."/".$section_name;?>"><?php echo $details['icon'];?></a>
+                    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4><?php echo $SectionName;?></h4>
+                            </div>
+                            <div class="card-body text-center">
+                            	<a class="btn btn-lg btn-outline-fsg controller-index-link" href="/<?php echo $con_index."/".$section_name;?>"><?php echo $details['icon'];?></a>
+                            </div>
                         </div>
                     </div>
+
                 <?php endif;?>
             <?php endforeach;?>
         </div>
