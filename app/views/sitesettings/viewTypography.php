cd58a5b20258ac555ad39eecce6f427dbacf56e8
marxolly <mark@solly.com.au>
2022-07-15 14:07:10 +1000

    Update viewTypography.php

--- app/views/sitesettings/viewTypography.php
+++ app/views/sitesettings/viewTypography.php
@@ -60,7 +60,7 @@ $c = 1;
                 <h3>Two Columns</h3>
                 <div class="col-md-6">
                     <div class="card border-primary mb-3">
-                        <div class="card-header border-bottom-primary bg-primary">Primary Card</div>
+                        <div class="card-header border-bottom-primary">Primary Card</div>
                         <div class="card-body">
                             <h5 class="card-title">Primary card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
