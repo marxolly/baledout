7d4c3591bdb50cfefb425654f39a626a592884ab
marxolly <mark@solly.com.au>
2022-07-21 15:34:28 +1000

    Update viewTypography.php

--- app/views/sitesettings/viewTypography.php
+++ app/views/sitesettings/viewTypography.php
@@ -60,19 +60,23 @@ $c = 1;
                 <h3>Two Columns On All But Phone</h3>
             </div>
             <div class="row row-cols-1 row-cols-sm-2">
-                <div class="card border-primary h-100">
-                    <div class="card-header border-primary bg-primary text-white">Primary Card<br><br>more..</div>
-                    <div class="card-body">
-                        <h5 class="card-title text-primary">Primary card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div class="col">
+                    <div class="card border-primary h-100">
+                        <div class="card-header border-primary bg-primary text-white">Primary Card<br><br>more..</div>
+                        <div class="card-body">
+                            <h5 class="card-title text-primary">Primary card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
                     </div>
                 </div>
-                <div class="card border-success h-100">
-                    <div class="card-header border-success bg-success text-white">Success Card</div>
-                    <div class="card-body">
-                        <h5 class="card-title text-success">Success card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div class="col">
+                    <div class="card border-success h-100">
+                        <div class="card-header border-success bg-success text-white">Success Card</div>
+                        <div class="card-body">
+                            <h5 class="card-title text-success">Success card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
                     </div>
                 </div>
             </div>
