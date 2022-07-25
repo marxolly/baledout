b377bf319de3e735a4538228fdfd9ddeebdc6cad
marxolly <mark@solly.com.au>
2022-07-25 12:30:14 +1000

    Update controllerIndex.php

--- app/views/index/controllerIndex.php
+++ app/views/index/controllerIndex.php
@@ -24,7 +24,7 @@ $resource = strtolower(str_replace(" ", "", $page_name));
                 //echo "<p>Checking Action: $action</p>";
                 if(Permission::check($role, $resource, $action, [], false)):?>
                     <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-3">
-                        <div class="card">
+                        <div class="card indexpagecard">
                             <div class="card-header">
                                 <h4><?php echo $SectionName;?></h4>
                             </div>
@@ -33,7 +33,6 @@ $resource = strtolower(str_replace(" ", "", $page_name));
                             </div>
                         </div>
                     </div>
-
                 <?php endif;?>
             <?php endforeach;?>
         </div>
