bc57b00431f4ee701db82beaa8114f6c2506d429
marxolly <mark@solly.com.au>
2022-07-27 15:12:56 +1000

    Update common.js

--- public_html/scripts/common.js
+++ public_html/scripts/common.js
@@ -111,12 +111,12 @@ var scroller = {
         if( $(window).scrollTop() > $nav_holder.height() )
         {
             $('div#user_info img.img-user').css({height: '3rem'});
-            $('div#user_info').css({marginTop: '-1rem'})
+            //$('div#user_info').css({marginTop: '-1rem'})
         }
         else
         {
             $('div#user_info img.img-user').css({height: '4rem'});
-            $('div#user_info').css({marginTop: '1rem'})
+            //$('div#user_info').css({marginTop: '1rem'})
         }
     },
     cardsInView: function(){
