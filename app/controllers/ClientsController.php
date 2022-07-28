eef261a115e91d5a01f25a98c85d4e4af88a8a06
marxolly <mark@solly.com.au>
2022-07-28 15:27:54 +1000

    Update ClientsController.php

---
+++ app/controllers/ClientsController.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Clients controller
+ *
+
+ Manages Clients
+
+ * @author     Mark Solly <mark@baledout.com.au>
+ */
+
+class ClientsController extends Controller
+{
+    public function beforeAction()
+    {
+        parent::beforeAction();
+        $this->Security->config("form", [ 'fields' => ['csrf_token']]);
+    }
+
+    public function index()
+    {
+        //set the page name for menu display
+        Config::setJsConfig('curPage', 'jobs-index');
+        parent::displayIndex(get_class());
+    }
+
+    public function isAuthorized()
+    {
+        $action = $this->request->param('action');
+        //$role = Session::getUserRole();
+        $role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
+        $resource = "clients";
+        //only for admin
+        Permission::allow(['admin', 'super admin'], $resource, "*");
+
+
+
+        return Permission::check($role, $resource, $action);
+    }
+}
