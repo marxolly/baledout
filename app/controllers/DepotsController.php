dc1d7db5d8dee5f50dcf8cf033eed331ba37e5cf
marxolly <mark@solly.com.au>
2022-07-28 15:44:44 +1000

    Create DepotsController.php

---
+++ app/controllers/DepotsController.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Depots controller
+ *
+
+ Manages Depots For hire and DeHire
+
+ * @author     Mark Solly <mark@baledout.com.au>
+ */
+
+class DepotController extends Controller
+{
+    public function beforeAction()
+    {
+        parent::beforeAction();
+        $this->Security->config("form", [ 'fields' => ['csrf_token']]);
+    }
+
+    public function index()
+    {
+        //set the page name for menu display
+        Config::setJsConfig('curPage', 'depots-index');
+        parent::displayIndex(get_class());
+    }
+
+    public function isAuthorized()
+    {
+        $action = $this->request->param('action');
+        //$role = Session::getUserRole();
+        $role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
+        $resource = "depots";
+        //only for admin
+        Permission::allow(['admin', 'super admin'], $resource, "*");
+
+
+
+        return Permission::check($role, $resource, $action);
+    }
+}
