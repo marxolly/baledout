01f590ede468739198601ad87001053b652ad127
marxolly <mark@solly.com.au>
2022-07-28 15:21:20 +1000

    Create JobsController.php

---
+++ app/controllers/JobsController.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Jobs controller
+ *
+
+ Manages Production Jobs
+
+ * @author     Mark Solly <mark.solly@fsg.com.au>
+ */
+
+class JobsController extends Controller
+{
+    public function beforeAction()
+    {
+        parent::beforeAction();
+        $this->Security->config("form", [ 'fields' => ['csrf_token']]);
+    }
+
+    public function index()
+    {
+        //set the page name for menu display
+        Config::setJsConfig('curPage', 'jobs-index');
+        parent::displayIndex(get_class());
+    }
+
+    public function isAuthorized()
+    {
+        $action = $this->request->param('action');
+        //$role = Session::getUserRole();
+        $role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
+        $resource = "jobs";
+        //only for admin
+        Permission::allow(['admin', 'super admin'], $resource, "*");
+
+
+
+        return Permission::check($role, $resource, $action);
+    }
+}
