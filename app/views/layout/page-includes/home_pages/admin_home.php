10b44ca4dc708241e67fefda5c18018bfdfbb4ea
marxolly <mark@solly.com.au>
2022-07-29 10:41:53 +1000

    Update admin_home.php

--- app/views/layout/page-includes/home_pages/admin_home.php
+++ app/views/layout/page-includes/home_pages/admin_home.php
@@ -43,7 +43,7 @@ $cc = 5;
             <div class="card-body">
                 <div class="quicklink">
                     <a href="#">
-                        <span class="fa-layers fa-fw"><i class="fa-duotone fa-truck-container"></i><i class="fa-solid fa-plus" data-fa-transform="shrink-5 up-4 right-12"></i></span>&nbsp;
+                        <span class="fa-layers fa-fw"><i class="fa-duotone fa-truck-container"></i><i class="fa-solid fa-plus" data-fa-transform="shrink-8 up-5 right-10"></i></span>&nbsp;
                         Add A Job
                     </a>
                 </div>
