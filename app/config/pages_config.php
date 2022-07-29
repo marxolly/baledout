9029747e18831bab75ed65c8d1b1ac415f7a6073
marxolly <mark@solly.com.au>
2022-07-29 11:23:05 +1000

    Update pages_config.php

--- app/config/pages_config.php
+++ app/config/pages_config.php
@@ -38,7 +38,7 @@ $admin = array(
         ],
         'view-jobs'   => [
             'display'   => true,
-            'icon'      => '<div class="fa-3x"><span class="fa-layers fa-fw"><i class="fa-duotone fa-truck-container"></i><i class="fa-solid fa-binoculars" data-fa-transform="shrink-10 rotate-30"></i></span></div>'
+            'icon'      => '<div class="fa-3x"><span class="fa-layers fa-fw"><i class="fa-duotone fa-truck-container"></i><i class="fa-solid fa-binoculars" data-fa-transform="shrink-10 up-2 left-2 rotate-30"></i></span></div>'
         ],
         'search-jobs' => [
             'display'   => true,
