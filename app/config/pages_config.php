419929bc5c2c1ef97a9c8a2b19481b90059a8fdc
marxolly <mark@solly.com.au>
2022-07-29 15:18:45 +1000

    Update pages_config.php

--- app/config/pages_config.php
+++ app/config/pages_config.php
@@ -62,7 +62,7 @@ $admin = array(
         ],
         'view-clients'   => [
             'display'   => true,
-            'icon'      => '<div class="fa-3x"><span class="fa-layers fa-fw"><i class="fa-duotone fa-user-tie"></i><i class="fa-solid fa-binoculars" data-fa-transform="shrink-10 up-3 left-3 rotate-30"></i></span></div>'
+            'icon'      => '<div class="fa-3x"><span class="fa-layers fa-fw"><i class="fa-duotone fa-user-tie"></i><i class="fa-solid fa-binoculars" data-fa-transform="shrink-10 up-5 right-3 rotate-30"></i></span></div>'
         ],
     ),
     'depots'    => array(
