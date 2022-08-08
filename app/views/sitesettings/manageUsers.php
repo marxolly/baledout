cfb04607e1d18f10c16519c62d1778826860d4d4
marxolly <mark@solly.com.au>
2022-08-09 09:38:11 +1000

    Update manageUsers.php

--- app/views/sitesettings/manageUsers.php
+++ app/views/sitesettings/manageUsers.php
@@ -5,7 +5,7 @@
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div class="row">
-            <div class="col-lg-3 col-md-6"><p><a href="/user/add-user" class="btn btn-outline-bo">Add New User</a></p></div>
+            <div class="col-lg-3 col-md-6"><p><a href="/site-settings/add-user" class="btn btn-outline-bo">Add New User</a></p></div>
             <div class="col-lg-3 col-md-6"><p><a href="/site-settings/manage-users/" class="btn btn-outline-success">View Only Active Users</a></p></div>
             <div class="col-lg-3 col-md-6"><p><a href="/site-settings/manage-users/active=0" class="btn btn-outline-danger">View Only Inactive Users</a></p></div>
             <div class="col-lg-3 col-md-6"><p><a href="/site-settings/manage-users/active=-1" class="btn btn-outline-warning">View All Users</a></p></div>
