5e58258887bdea1ade89705977826d7c3db4615d
marxolly <mark@solly.com.au>
2022-08-09 14:13:56 +1000

    Update form-top.php

--- app/views/layout/page-includes/form-top.php
+++ app/views/layout/page-includes/form-top.php
@@ -8,18 +8,17 @@
         <?php endif; ?>
         <a id="form_top"></a>
         <?php if(Form::$num_errors > 0) :?>
-        	<div class='errorbox'>Sorry, some errors were found with the form. Please correct where shown and re-submit.</div>
-                <div class="errorbox">
-                    <div class="row">
-                        <div class="col-4 text-right">
-                            <i class="fad fa-exclamation-triangle fa-6x"></i>
-                        </div>
-                        <div class="col-8">
-                            <h2>Sorry, some errors were found with the form.</h2>
-                            <p>Please correct where shown and re-submit.</p>
-                        </div>
+            <div class="errorbox">
+                <div class="row">
+                    <div class="col-4 col-lg-2 text-end">
+                        <i class="fad fa-exclamation-triangle fa-6x"></i>
+                    </div>
+                    <div class="col-8 col-lg-10">
+                        <h2>Sorry, some errors were found with the form.</h2>
+                        <p>Please correct where shown and re-submit.</p>
                     </div>
                 </div>
+            </div>
         <?php endif ?>
         <!--p class="text-info">fields marked <sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> are required</p-->
     </div>
