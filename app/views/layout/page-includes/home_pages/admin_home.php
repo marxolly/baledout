a31fee90435a595f6ab7f3421dcaab1678ba2f02
marxolly <mark@solly.com.au>
2022-08-15 15:03:20 +1000

    Update admin_home.php

--- app/views/layout/page-includes/home_pages/admin_home.php
+++ app/views/layout/page-includes/home_pages/admin_home.php
@@ -9,30 +9,7 @@ $cc = 5;
             <div class="card-header text-center">Jobs</div>
             <div class="card-body">
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4">
-                    <?php for($c = 1; $c <= 5; $c++):
-                        $logo_path = DOC_ROOT.'/images/client_logos/tn_default.png';?>
-                        <div class="col mb-3">
-                            <div class="card homepagecard">
-                                <div class="card-header">
-                                    <div class="row">
-                                        <div class="col-5 d-sm-none d-md-block col-md-5">
-                                            <img src="/images/client_logos/tn_default.png" alt="client logo" class="img-thumbnail" />
-                                        </div>
-                                        <div class="col-7 col-sm-12 col-md-7">
-                                            <h5 class="d-none d-md-block">Client Number <?php echo $c;?> Name</h5>
-                                            <h4 class="d-md-none">Client Number <?php echo $c;?> Name</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    Count the jobs
-                                </div>
-                                <div class="card-footer text-center">
-                                    <a class="btn btn-outline-bo" href="#">Manage Jobs</a>
-                                </div>
-                            </div>
-                        </div>
-                    <?php endfor;?>
+                    <div id="calendar"></div>
                 </div>
             </div>
         </div>
