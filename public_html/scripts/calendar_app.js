5eb4692f165f41b40f0a85ae984fff24ca7993af
marxolly <mark@solly.com.au>
2022-08-16 15:59:10 +1000

    Update calendar_app.js

--- public_html/scripts/calendar_app.js
+++ public_html/scripts/calendar_app.js
@@ -44,6 +44,24 @@
         navbarRange.textContent = getNavbarRange(rangeStart, rangeEnd, cal.getViewName());
     }
 
+    function getNavbarRange(tzStart, tzEnd, viewType) {
+        var start = tzStart.toDate();
+        var end = tzEnd.toDate();
+        var middle;
+        if (viewType === 'month') {
+            middle = new Date(start.getTime() + (end.getTime() - start.getTime()) / 2);
+
+            return moment(middle).format('YYYY-MM');
+        }
+        if (viewType === 'day') {
+            return moment(start).format('YYYY-MM-DD');
+        }
+        if (viewType === 'week') {
+            return moment(start).format('YYYY-MM-DD') + ' ~ ' + moment(end).format('YYYY-MM-DD');
+        }
+        throw new Error('no view type');
+    }
+
     function setSelectViewValue()
     {
         var viewName = cal.getViewName();
