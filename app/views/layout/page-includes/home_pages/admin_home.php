b56c53b38d84f1758b510865d7d186502708cb26
marxolly <mark@solly.com.au>
2022-08-16 14:08:16 +1000

    Update admin_home.php

--- app/views/layout/page-includes/home_pages/admin_home.php
+++ app/views/layout/page-includes/home_pages/admin_home.php
@@ -20,14 +20,16 @@ $cc = 5;
                         <button id="today" class="btn btn-outline-bo mb-3">Today</button>
                     </div>
                     <div class="col-md-5">
-                         <div id="prev" class="col-1">
-                            <i class="fa-light fa-circle-chevron-left"></i>
-                        </div>
-                        <div class="col-3 text-center">
-                            <span class="navbar--range">gggg</span>
-                        </div>
-                        <div id="next" class="col-1">
-                            <i class="fa-light fa-circle-chevron-right"></i>
+                        <div class="row">
+                             <div id="prev" class="col-1">
+                                <i class="fa-light fa-circle-chevron-left"></i>
+                            </div>
+                            <div class="col-3 text-center">
+                                <span class="navbar--range">gggg</span>
+                            </div>
+                            <div id="next" class="col-1">
+                                <i class="fa-light fa-circle-chevron-right"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
