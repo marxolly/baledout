4ac53880197b12b4f8f902f46cbe4734113bb247
marxolly <mark@solly.com.au>
2022-08-18 09:35:25 +1000

    Update admin_home.php

--- app/views/layout/page-includes/home_pages/admin_home.php
+++ app/views/layout/page-includes/home_pages/admin_home.php
@@ -4,8 +4,33 @@ $cc = 5;
 <input type="hidden" id="admin_from_value" value="<?php echo strtotime('last friday', strtotime('-3 months'));?>" />
 <input type="hidden" id="admin_to_value" value="<?php echo strtotime('last friday', strtotime('tomorrow'));?>" />
 <div class="row">
-    <div class="col col-md-12 col-xl-10 offset-xl-1">
-        <div class="card cardholder jobscardholder">
+    <div class="col-md-12 col-xl-2 offset-xl-10">
+        <div class="card cardholder quicklinkscardholder h-100">
+            <div class="card-header text-center">Quick Links</div>
+            <div class="card-body">
+                <div class="quicklink">
+                    <a href="/jobs/add-job">
+                        <span class="fa-layers fa-fw"><i class="fa-duotone fa-truck-container"></i><i class="fa-solid fa-plus" data-fa-transform="shrink-8 up-5 right-10"></i></span>&nbsp;
+                        Add A Job
+                    </a>
+                </div>
+                <div class="quicklink">
+                    <a href="/clients/add-client">
+                        <span class="fa-layers fa-fw"><i class="fa-duotone fa-user-tie"></i><i class="fa-solid fa-plus" data-fa-transform="shrink-8 up-5 right-8"></i></span>&nbsp;
+                        Add A Client
+                    </a>
+                </div>
+                <div class="quicklink">
+                    <a href="depots/add-depot">
+                        <span class="fa-layers fa-fw"><i class="fa-duotone fa-warehouse-full"></i><i class="fa-solid fa-plus" data-fa-transform="shrink-8 up-6 right-12"></i></span>&nbsp;
+                        Add A Depot
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col col-md-12 col-xl-10">
+        <div class="card cardholder jobscardholder h-100">
             <div class="card-header text-center">Jobs</div>
             <div class="card-body">
                 <div class="row">
@@ -37,29 +62,4 @@ $cc = 5;
             </div>
         </div>
     </div>
-    <!--div class="col-md-4 col-xl-3 d-none d-md-block">
-        <div class="card cardholder quicklinkscardholder h-100">
-            <div class="card-header text-center">Quick Links</div>
-            <div class="card-body">
-                <div class="quicklink">
-                    <a href="/jobs/add-job">
-                        <span class="fa-layers fa-fw"><i class="fa-duotone fa-truck-container"></i><i class="fa-solid fa-plus" data-fa-transform="shrink-8 up-5 right-10"></i></span>&nbsp;
-                        Add A Job
-                    </a>
-                </div>
-                <div class="quicklink">
-                    <a href="/clients/add-client">
-                        <span class="fa-layers fa-fw"><i class="fa-duotone fa-user-tie"></i><i class="fa-solid fa-plus" data-fa-transform="shrink-8 up-5 right-8"></i></span>&nbsp;
-                        Add A Client
-                    </a>
-                </div>
-                <div class="quicklink">
-                    <a href="depots/add-depot">
-                        <span class="fa-layers fa-fw"><i class="fa-duotone fa-warehouse-full"></i><i class="fa-solid fa-plus" data-fa-transform="shrink-8 up-6 right-12"></i></span>&nbsp;
-                        Add A Depot
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div-->
 </div>
