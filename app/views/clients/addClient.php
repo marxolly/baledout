3c6e5aa83e3602779e12f60f5f486fceb35f8244
marxolly <mark@solly.com.au>
2022-08-18 14:17:41 +1000

    Update addClient.php

--- app/views/clients/addClient.php
+++ app/views/clients/addClient.php
@@ -2,7 +2,7 @@
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <div class="row">
-            <div class="col">
+            <div class="col text-end">
                 <p><a href="/clients/view-clients/" class="btn btn-outline-bo">Return to Client List</a></p>
             </div>
         </div>
