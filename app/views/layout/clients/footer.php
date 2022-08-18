f8bfbd45e496c49db5b61a01c20c7f2917c01139
marxolly <mark@solly.com.au>
2022-08-19 09:38:50 +1000

    Update footer.php

--- app/views/layout/clients/footer.php
+++ app/views/layout/clients/footer.php
@@ -6,12 +6,37 @@
             var actions = {
                 common: {
                     init: function(){
-
+                        autoCompleter.addressAutoComplete($('#address'));
+                        autoCompleter.suburbAutoComplete($('#suburb'));
+                    },
+                    addContact: function(){
+                        $("a.add-contact").click(function(e){
+                            e.preventDefault();
+                            var contact_count = $("div#contacts_holder div.acontact").length;
+                            //console.log('packages: '+contact_count);
+                            var data = {
+                                i: contact_count
+                            }
+                            $.post('/ajaxfunctions/addFinisherContact', data, function(d){
+                                $('div#contacts_holder').append(d.html);
+                            });
+                        });
+                        $("a#remove-all-contacts").click(function(e){
+                            e.preventDefault();
+                            $('div#contacts_holder div.acontact').not(':first').remove();
+                        });
                     }
                 },
                 'add-client': {
                     init: function(){
-                        
+                        actions.common.init();
+                        actions.common.addContact();
+                        $('form#client_add').submit(function(e){
+                            if($(this).valid())
+                            {
+                                $.blockUI({ message: '<div style="height:140px; padding-top:20px;"><h2>Adding Client...</h2></div>' });
+                            }
+                        });
                     }
                 }
 
