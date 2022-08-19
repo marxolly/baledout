acee1e793d5e6dd67310202d20d79ae04b1926f6
marxolly <mark@solly.com.au>
2022-08-19 11:20:11 +1000

    Update common.js

--- public_html/scripts/common.js
+++ public_html/scripts/common.js
@@ -805,8 +805,8 @@ var autoCompleter = {
                 $('#'+prefix+'address').val(ui.item.address).change();
                 $('#'+prefix+'address2').val(ui.item.address_2).change();
                 $('#'+prefix+'suburb').val(ui.item.suburb).change();
-                $('#'+prefix+'state').val(ui.item.state).change();
-                $('#'+prefix+'postcode').val(ui.item.postcode).change().selectpicker('refresh');
+                $('#'+prefix+'state').val(ui.item.state).change().selectpicker('refresh');
+                $('#'+prefix+'postcode').val(ui.item.postcode).change();
                 $('#'+prefix+'country').val("AU").change();
                 $(event.target).val(ui.item.address);
                 console.log("will change value of #"+prefix+'state to '+ui.item.state);
