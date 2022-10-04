bcd4b8e1f0f2ce87d9c58118296850ae938565cc
marxolly <mark@solly.com.au>
2022-10-04 12:55:52 +1100

    Update viewClients.php

--- app/views/clients/viewClients.php
+++ app/views/clients/viewClients.php
@@ -10,8 +10,26 @@
                 <p class="text-right"><?php echo $link_text;?></p>
             </div>
         </div>
-        <?php if(!count($clients)):?>
+        <?php if(count($clients)):?>
+            <div id="waiting" class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Drawing Table..</h2>
+                    <p>May take a few moments</p>
+                    <img class='loading' src='/images/preloader.gif' alt='loading...' />
+                </div>
+            </div>
+            <div class="row" id="table_holder" style="display:none">
+                <div class="col-12">
+                    <table id="client_list_table" class="table-striped table-hover" width="100%">
+                        <thead>
 
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         <?php else:?>
             <div class="row">
                 <div class="col-lg-12">
