48f97b2515b9148297ff9c528234e24467bd5e30
marxolly <mark@solly.com.au>
2022-10-04 12:46:34 +1100

    Update viewClients.php

--- app/views/clients/viewClients.php
+++ app/views/clients/viewClients.php
@@ -5,5 +5,22 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xxl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <div class="row">
+            <div class="col">
+                <p class="text-right"><?php echo $link_text;?></p>
+            </div>
+        </div>
+        <?php if(!count($clients)):?>
+
+        <?php else:?>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="errorbox">
+                        <h2>No Clients Listed</h2>
+                        <p>You might want to <a href="/clients/add-client/">add one</a></p>
+                    </div>
+                </div>
+            </div>
+        <?php endif;?>
     </div>
 </div>
