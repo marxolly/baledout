46d489fe63776a51ea61ca38ed27f448a70a9211
marxolly <mark@solly.com.au>
2022-10-11 09:49:05 +1100

    Update address_nr.php

--- app/views/layout/page-includes/forms/address_nr.php
+++ app/views/layout/page-includes/forms/address_nr.php
@@ -6,7 +6,7 @@ if(!isset($prefix))
     <label class="col-md-3">Address Line 1</label>
     <div class="col-md-4">
         <input type="text" class="form-control" name="<?php echo $prefix;?>address" id="<?php echo $prefix;?>address" value="<?php echo ${$prefix."address"};?>" />
-        <?php echo Form::displayError('address');?>
+        <?php echo Form::displayError($prefix.'address');?>
     </div>
 </div>
 <div class="form-group row mb-3">
@@ -19,20 +19,20 @@ if(!isset($prefix))
     <label class="col-md-3">Suburb/Town</label>
     <div class="col-md-4">
         <input type="text" class="form-control" name="<?php echo $prefix;?>suburb" id="<?php echo $prefix;?>suburb" value="<?php echo ${$prefix."suburb"};?>" />
-        <?php echo Form::displayError('suburb');?>
+        <?php echo Form::displayError($prefix.'suburb');?>
     </div>
 </div>
 <div class="form-group row mb-3">
     <label class="col-md-3">State</label>
     <div class="col-md-4">
         <select id="<?php echo $prefix;?>state" name="<?php echo $prefix;?>state" class="form-control selectpicker" data-style="btn-outline-bo"><option value="0">--Select One--</option><?php echo Utility::getStateSelect(${$prefix."state"});?></select>
-        <?php echo Form::displayError('state');?>
+        <?php echo Form::displayError($prefix.'state');?>
     </div>
 </div>
 <div class="form-group row mb-3">
     <label class="col-md-3 ">Postcode</label>
     <div class="col-md-4">
         <input type="text" class="form-control" name="<?php echo $prefix;?>postcode" id="<?php echo $prefix;?>postcode" value="<?php echo ${$prefix."postcode"};?>" />
-        <?php echo Form::displayError('postcode');?>
+        <?php echo Form::displayError($prefix.'postcode');?>
     </div>
 </div>
