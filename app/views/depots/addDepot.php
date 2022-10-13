f315149e8c907634a19971591d4bde2dfb6c4643
marxolly <mark@solly.com.au>
2022-10-13 14:50:17 +1100

    Update addDepot.php

--- app/views/depots/addDepot.php
+++ app/views/depots/addDepot.php
@@ -83,6 +83,21 @@ $postcode = Form::value('postcode');
                         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/forms/add_customer_contact.php");?>
                     </div>
                 </div>
+                <div class="p-3 pb-0 mb-2 rounded-top form-section-holder">
+                    <div class="row">
+                        <div class="col">
+                            <h3>Save Depot</h3>
+                        </div>
+                    </div>
+                    <div class="p-3 light-grey mb-3">
+                        <div class="form-group row">
+                            <div class="col-md-4 offset-md-3 text-center text-md-start">
+                                <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+                                <button type="submit" class="btn btn-outline-bo" id="submitter">Add This Depot</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </form>
     </div>
