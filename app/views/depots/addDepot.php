d2d7d2b619203aeccccc85a5bb1a3aec44e2c305
marxolly <mark@solly.com.au>
2022-10-13 14:40:02 +1100

    Update addDepot.php

--- app/views/depots/addDepot.php
+++ app/views/depots/addDepot.php
@@ -25,6 +25,22 @@ $postcode = Form::value('postcode');
                             <?php include(Config::get('VIEWS_PATH')."layout/page-includes/forms/required_fields.php");?>
                         </div>
                     </div>
+                    <div class="p-3 light-grey mb-3">
+                        <div class="form-group row mb-3">
+                            <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup>Depot Name</label>
+                            <div class="col-md-4">
+                                <input type="text" class="form-control required" name="depot_name" id="depot_name" value="<?php echo Form::value('depot_name');?>" />
+                                <?php echo Form::displayError('depot_name');?>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-3">
+                            <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup>Depot Abbreviation<br><span class="inst">abbreviations must be unique</span></label>
+                            <div class="col-md-4">
+                                <input type="text" class="form-control required" name="abbreviation" id="abbreviation" value="<?php echo Form::value('abbreviation');?>" />
+                                <?php echo Form::displayError('abbreviation');?>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </form>
