380f98900f4a89162f988fca410dabe26636f3ba
marxolly <mark@solly.com.au>
2022-10-18 11:06:48 +1100

    Update FormController.php

--- app/controllers/FormController.php
+++ app/controllers/FormController.php
@@ -79,7 +79,7 @@ class FormController extends Controller {
                 }
             }
         }
-        echo "<pre>POST DATA",print_r($post_data),"</pre>"; die();
+        //echo "<pre>POST DATA",print_r($post_data),"</pre>"; die();
         $this->depotDataValidate($post_data);
         if(Form::$num_errors > 0)		/* Errors exist, have user correct them */
         {
