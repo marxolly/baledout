d88967eeebdad03c1005a9044a15c1c7d8c0ee5e
marxolly <mark@solly.com.au>
2022-10-19 09:53:53 +1100

    Update editDepot.php

--- app/views/depots/editDepot.php
+++ app/views/depots/editDepot.php
@@ -47,5 +47,53 @@ else
                 <p><a href="/depots/view-depots/" class="btn btn-outline-bo">Return to Depot List</a></p>
             </div>
         </div>
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
+        <?php echo Form::displayError('general');?>
+        <?php //echo "<pre>",print_r($depot),"</pre>";?>
+        <form id="depot_edit" method="post" action="/form/procDepotEdit">
+            <div class="row">
+                <div class="p-3 pb-0 mb-2 rounded-top form-section-holder">
+                    <div class="row">
+                        <div class="col">
+                            <h3>Depot Details</h3>
+                            <?php include(Config::get('VIEWS_PATH')."layout/page-includes/forms/required_fields.php");?>
+                        </div>
+                    </div>
+                    <div class="p-3 light-grey mb-3">
+                        <div class="form-group row mb-3">
+                            <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup>Depot Name</label>
+                            <div class="col-md-4">
+                                <input type="text" class="form-control required" name="depot_name" id="depot_name" value="<?php echo $depot_name;?>" />
+                                <?php echo Form::displayError('depot_name');?>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-3">
+                            <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup>Depot Abbreviation<br><span class="inst">abbreviations must be unique</span></label>
+                            <div class="col-md-4">
+                                <input type="text" class="form-control required" name="abbreviation" id="abbreviation" value="<?php echo $abbreviation;?>" />
+                                <?php echo Form::displayError('abbreviation');?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-3 pb-0 mb-2 rounded-top form-section-holder">
+                    <div class="row">
+                        <div class="col">
+                            <h3>Save Changes</h3>
+                        </div>
+                    </div>
+                    <div class="p-3 light-grey mb-3">
+                        <div class="form-group row">
+                            <div class="col-md-4 offset-md-3 text-center text-md-start">
+                                <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" >
+                                <input type="hidden" name="depot_id" value="<?php echo $depot['id'];?>" >
+                                <input type="hidden" name="current_abbreviation" id="current_abbreviation" value="<?php echo $depot['abbreviation']; ?>" >
+                                <button type="submit" class="btn btn-outline-bo" id="submitter">Edit This Depot</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
