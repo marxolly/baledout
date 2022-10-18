6692afd507e9e97aea69f8568a68579058abfb4b
marxolly <mark@solly.com.au>
2022-10-18 15:01:19 +1100

    Update Depot.php

--- app/models/Depot.php
+++ app/models/Depot.php
@@ -101,7 +101,7 @@
                 {$this->contacts_table} dc ON dc.depot_id = d.id LEFT JOIN
                 {$this->addresses_table} da ON d.address = da.id
             WHERE
-                dc.active = 1
+                dc.active = 1 OR ISNULL(dc.id)
         ";
         if($depot_id > 0)
             $q .= " AND d.id = $depot_id";
