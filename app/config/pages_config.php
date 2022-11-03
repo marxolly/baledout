201666c5ddc28bd60d30858b7e0a37bf7465e417
marxolly <mark@solly.com.au>
2022-11-03 15:54:58 +1100

    Update pages_config.php

--- app/config/pages_config.php
+++ app/config/pages_config.php
@@ -100,13 +100,11 @@ $admin = array(
         'add-driver'    => [
             'display'   => true,
             'icon'      => '
-                <div class="fa-3x">
-                    <span class="fa-stack fa-1x">
-                        <i class="fa-duotone fa-user fa-stack-1x"></i>
-                        <i class="fa-regular fa-stack-1x fa-steering-wheel fa-inverse" data-fa-transform="shrink-6 down-4"></i>
-                        <i class="fa-solid fa-plus fa-stack-1x" data-fa-transform="shrink-8 up-6 right-8"></i>
-                    </span>
-                </div>
+                <span class="fa-stack fa-3x">
+                    <i class="fa-duotone fa-user fa-stack-1x"></i>
+                    <i class="fa-regular fa-stack-1x fa-steering-wheel fa-inverse" data-fa-transform="shrink-6 down-4"></i>
+                    <i class="fa-solid fa-plus fa-stack-1x" data-fa-transform="shrink-8 up-6 right-8"></i>
+                </span>
             '
         ],
         'view-drivers'    => [
