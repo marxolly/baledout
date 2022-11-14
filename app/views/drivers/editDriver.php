0d4cf2b7d281ff402f26d79cd9a4c8219239b7fe
marxolly <mark@solly.com.au>
2022-11-14 11:19:25 +1100

    Update editDriver.php

--- app/views/drivers/editDriver.php
+++ app/views/drivers/editDriver.php
@@ -44,7 +44,13 @@ $ppp = DOC_ROOT.'/images/profile_pictures/'.$driver['profile_picture'];
                         </div>
                     </div>
                     <div class="p-3 light-grey mb-3">
-
+                        <div class="form-group row mb-3">
+                            <label class="col-md-3"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Name</label>
+                            <div class="col-md-4">
+                                <input type="text" class="form-control required" name="name" id="name" value="<?php echo $name;?>" />
+                                <?php echo Form::displayError('name');?>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
