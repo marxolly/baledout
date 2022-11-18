c71f12d128e50ea7eb4090be3d6bff83bc6f906c
marxolly <mark@solly.com.au>
2022-11-18 12:04:17 +1100

    Update Utility.php

--- app/utility/Utility.php
+++ app/utility/Utility.php
@@ -23,6 +23,7 @@ class Utility{
         $abn = preg_replace('/\s+/', '', $abn);
         //format as "XX XXX XXX XXX"
         $array = str_split($abn);
+        var_dump($array);die();
         $formatted_abn = array_slice($array,0,2)." ".array_slice($array,2,3)." ".array_slice($array,5,3)." ".array_slice($array,8,3);
         return $formatted_abn;
     }
